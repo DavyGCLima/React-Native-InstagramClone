1e6ba7524919015613d2c4c4232b299fbcf0dd7c
DavyGCLima <davy.g.c.lima@gmail.com>
2019-08-19 10:28:22 -0300

    Carregamento infinito, estilização e indicação de carregamento

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -2,30 +2,53 @@ import React, { useState, useEffect } from 'react';
 import { View } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 
-import { PostImage, Header, Avatar, Name, Description } from './styles';
+import { PostImage, Header, Avatar, Name, Description, Loading } from './styles';
 
 export default function Feed() {
     const [feed, setFeed] = useState([]);
+    const [page, setPage] = useState(1);
+    const [total, setTotal] = useState(0);
+    const [loading, setLoading] = useState(false);
+    const [refresing, setRefreshing] = useState(false);
+    
+    async function loadPage(pageNumber = page, shouldRefresh = false) {
+        if(total && pageNumber > total) return;
+
+        setLoading(true);
+
+        const response = await fetch(
+            `http://localhost:3000/feed?_expand=author&_limit=5&_page=${pageNumber}`
+        );
+
+        const data = await response.json();
+        const totalItems = response.headers.get('X-Total-Count');
+
+        setTotal(Math.floor(totalItems / 5));
+         setFeed(shouldRefresh ? data : [...feed, ...data]);
+        setPage(pageNumber + 1)
+        setLoading(false);
+    }
 
     useEffect(() => {
-        async function loadFeed() {
-            const response = await fetch(
-                'http://localhost:3000/feed?_expand=author&_limit=5&_page=1'
-            );
-
-            const data = await response.json();
-
-            setFeed(data);
-        }
-
-        loadFeed();
+        loadPage();
     }, []);
+
+    async function refreshList() {
+        setRefreshing(true);
+        await loadPage(1, true);
+        setRefreshing(false);
+    }
 
     return(
         <View>
             <FlatList 
                 data={feed}
                 keyExtractor={post => String(post.id)}
+                onEndReached={() => loadPage()}
+                onEndReachedThreshold={0.1}
+                ListFooterComponent={loading && <Loading />}
+                onRefresh={refreshList}
+                refreshing={refreshing}
                 renderItem={({item}) => (
                     <Post>
                         <Header>
